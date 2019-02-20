bb8cfe67e7588cfaf32084c0342c1b323f38070d
liu <liu@ula.ve>
2019-02-20 12:09:32 -0300

    cambion

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 let posicion = ubicacion =>{
-	console.log(ubicacion.coords.latitud)
+	console.log(ubicacion.coords.latitude)
 	console.log(ubicacion.coords.longitude)
+	let p = document.createElement("p")
+	p.innerText = `Mi ubicacion es ${lat} y ${lon} `
+	document.body.appendChild(p)
 }
 navigator.geolocation.getCurrentPosition(posicion)
